4a898c3a256e87fe172320906907d6bd6096e488
Tara E. Sabella <tarasabella@gmail.com>
2018-09-11 16:21:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
  function johnLennonFacts() {
- var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice",];
- var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array += facts[i] + "!!!";
-    i++;
-  }
-  return array;
+  var array = [];
+  facts = [
+   "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice",
+];
+ var i = 0;
+    while (i < facts.length) {
+      array.push(facts[i] + "!!!");
+      i++;
+    }
+    return array;
 }
  function iLoveTheBeatles(number) {
   var array = [];
