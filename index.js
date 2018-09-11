60b81cc6b26ad7ee8754fe1099f06e710941de36
Tara E. Sabella <tarasabella@gmail.com>
2018-09-11 16:23:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
- function johnLennonFacts(facts) {
+ function johnLennonFacts() {
   var array = [];
   facts = [
-   "He was the last Beatle to learn to drive",
+  "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice",
-];
+  "He hated the sound of his own voice"];
  var i = 0;
     while (i < facts.length) {
       array.push(facts[i] + "!!!");
